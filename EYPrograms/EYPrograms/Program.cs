301bf36f16a0038840b2a52153a912253b04c6cc
Anusha <Anusha.Bhattacharya@gds.ey.com>
2023-02-16 17:52:47 +0000

    Change added

---
+++ EYPrograms/EYPrograms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace EYPrograms
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a1 = 10;
+			float a2 = 10.344f;
+			char ch = 'a';
+			string s1 = "EYclass";
+
+			Console.WriteLine(a1);
+			Console.WriteLine(a2);
+			Console.WriteLine(ch);
+			Console.WriteLine(s1);
+
+			Console.ReadLine();
+		}
+	}
+}
