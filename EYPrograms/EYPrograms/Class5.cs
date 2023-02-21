6a9d04a9c23078fd7f62a22bbbffa908eecbf97b
Anusha <Anusha.Bhattacharya@gds.ey.com>
2023-02-21 06:26:29 +0000

    Added

---
+++ EYPrograms/EYPrograms/Class5.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+
+
+//namespace EYPrograms
+//{
+//    class Class5
+//    {
+//        static void Main(string[] args)
+        
+            
+//                {
+                    
+                
+//                Console.WriteLine("Enter first number:");
+//                int a = int.Parse(Console.ReadLine());
+//                       Console.WriteLine("Enter second number:");
+    
+//                       int b = int.Parse(Console.ReadLine());
+//                Console.WriteLine("Enter operator:");
+
+//                char op = Convert.ToChar(Console.ReadLine());
+//                float res;
+//                    switch (op)
+//                    {
+//                        case '+':
+//                            res = a + b;
+//                            Console.WriteLine(res);
+//                            break;
+//                        case '-':
+//                            res = a - b;
+//                            Console.WriteLine(res);
+//                            break;
+
+//                    }
+//                    Console.WriteLine("Do you want to continue:");
+                   
+//                string ch = "yes";
+//            ch = Console.ReadLine();
+//            if (ch == "Y" || ch == "y" || ch == "YES" || ch == "yes")
+//            {
+                
+//            }
+          
+                
+            
+            
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
