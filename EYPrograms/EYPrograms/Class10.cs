17b602cb568cc05e44acca0fe556a1c3d08ceba1
Anusha <Anusha.Bhattacharya@gds.ey.com>
2023-02-24 07:32:09 +0000

    add

---
+++ EYPrograms/EYPrograms/Class10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+// c:\\demo file is there within c directory, i want to create demo1, demo2, demo3 directory within that
+namespace EYPrograms
+{
+    class Class10
+    {
+        static void Main(string[] args)
+        {
+            FileInfo f1 = new FileInfo("C:\\Users\\labuser\\Desktop\\DEMO");
+            Directory.CreateDirectory("C:\\Users\\labuser\\Desktop\\DEMO\\demo1");
+            Directory.CreateDirectory("C:\\Users\\abuser\\Desktop\\DEMO\\demo2");
+            Directory.CreateDirectory("C:\\Users\\labuser\\Desktop\\DEMO\\demo3");
+            Console.WriteLine("Directories within DEMO are: ");
+            foreach(var i in Directory.GetDirectories("C:\\Users\\labuser\\Desktop"))
+            {
+                Console.WriteLine(i);
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- EYPrograms/EYPrograms/Class9.cs
+++ EYPrograms/EYPrograms/Class9.cs
@@ -1,30 +1,30 @@
-﻿using System;
+﻿//using System;
 
-class Program
-{ 
+//class Program
+//{ 
 
 
-    public void Show(params int[] val)
-    {
-        for (int i = 0; i < val.Length; i++)
-        {
-            Console.WriteLine(val[i]);
-        }
-    }
-    public void Show1(params string[] val1)
-    {
-        for (var i = 0; i < val1.Length; i++)
-        {
-            Console.WriteLine(val1[i]);
-        }
-    }
-    //main function, execution entry point of program
-    static void Main(string[] args)
-    {
-        Program program = new Program();
-        program.Show(2, 4, 6, 8, 10, 12, 14);
-        Program program1 = new Program();
-        program1.Show1("aa", "bb");
-        Console.ReadLine();
-    }
-}
+//    public void Show(params int[] val)
+//    {
+//        for (int i = 0; i < val.Length; i++)
+//        {
+//            Console.WriteLine(val[i]);
+//        }
+//    }
+//    public void Show1(params string[] val1)
+//    {
+//        for (var i = 0; i < val1.Length; i++)
+//        {
+//            Console.WriteLine(val1[i]);
+//        }
+//    }
+//    //main function, execution entry point of program
+//    static void Main(string[] args)
+//    {
+//        Program program = new Program();
+//        program.Show(2, 4, 6, 8, 10, 12, 14);
+//        Program program1 = new Program();
+//        program1.Show1("aa", "bb");
+//        Console.ReadLine();
+//    }
+//}
