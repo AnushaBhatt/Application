88a68d3260acda7e5fde41cbd8839187e8155600
Anusha <Anusha.Bhattacharya@gds.ey.com>
2023-02-27 10:33:21 +0000

    addd

---
+++ EYPrograms/EYPrograms/Class11.cs
@@ -0,0 +1,123 @@
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading.Tasks;
+
+////namespace EYPrograms
+////{
+////    class india
+////    {
+////        static int custid = 100;
+////        static string custname;
+
+////        public void display()//by default all member functions are private
+////        {
+////            Console.WriteLine("Customer id is: " + custid);
+////        }
+////    }//if we keep void display it is not able to access elements 
+////    class UK
+////    {
+////        protected int empid;
+////        protected string empname;
+////    }
+////    class US
+////    {
+////        public int studid=10;
+////        public string studname;
+////    }
+////    class Class11
+////    {
+////        static void Main(string[] args)
+////        {
+////           india obj1 = new india();
+////          obj1.display();
+
+////           // UK obj2 = new UK();
+////           // obj2.//cannot access
+
+////                US obj3 = new US();
+////            obj3.studid = 7 ;
+////            Console.ReadLine();
+////        }
+////    }
+////}
+////single level inheritance
+//using System;
+
+//class vehicle // parent class 
+//{
+//    public void vehiclecolor()
+//    {
+//        Console.WriteLine("car colors available are : green, blue, red");
+
+//    }
+//}
+//class car : vehicle  //hierarchial inheritance
+//{
+//    public void musicsystem()
+//    {
+//        Console.WriteLine("Adding music system inside jeep");
+//    }
+//}
+//class scooter : car //scooter is child class and its multilevel inheritence
+//{
+//    public void wheel()
+//    {
+//        Console.WriteLine("car has two wheels");
+//    }
+//}
+//class jeep : car  //car is child class
+//{
+//    public void carcolor()
+//    {
+//        Console.WriteLine("car colors available are : blue, red");
+//    }
+//}
+//class myclass
+//{
+//    static void Main(string[] args)
+//    {
+//        jeep obj = new jeep();
+//        obj.carcolor();
+//        obj.vehiclecolor();
+//        obj.musicsystem();
+//        Console.ReadLine();
+//    }
+//}
+
+//access specifiers with inheritence
+using System;
+class india
+{
+    protected indiaid = 34;
+        public void function1()
+    {
+        Console.WriteLine("india class--function1()-- indiaid is " + indiaid);
+
+    }
+
+}
+class bangalore : india
+{
+    int bangid = 10;
+    public void function2()
+    {
+        Console.WriteLine("bangalore class--function2()-- bangid is " + bangid);
+    }
+}
+class tumkur : bangalore
+{
+    int tumkurid = 10;
+    public void function3()
+    {
+       Console.WriteLine("tumkur class--function3()-- tumkurid is " + tumkurid);
+    }
+}
+class mysore: india
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
--- EYPrograms/EYPrograms/Class6.cs
+++ EYPrograms/EYPrograms/Class6.cs
@@ -10,45 +10,46 @@
 //    {
 //        static void Main(string[] args)
 //        {
-//            Console.WriteLine("Matrix is:");
-//            int[,] arr1 = new int[2, 2];
-//            int i, j;
-//            arr1[0, 0] = 2;
-//            arr1[0, 1] = 3;
-//            arr1[1, 0] = 7;
-//            arr1[1, 1] = 6;
-//            for (i = 0; i < 2; i++)
-//            {
-//                for (j = 0; j < 2; j++)
-//                {
-//                    Console.Write(arr1[i, j] + "  ");
-//                }
-//                Console.WriteLine();
-//            }
-//            int a = 0;
             
-//            for(i=0; i<=1; i++)
-//            {
                 
-//                a = arr1[i, i] + a;
-               
 //            }
-//            Console.WriteLine("Addition of principal diagonal elements is: "+a);
-//            int b = 0;
-//                for(i=0; i<=1; i++)
-//                {
-//                    for(j=0; j<=1;j++)
-//                    {
-//                        b = arr1[i, j] + b;
+//            //int i, j;
+//            //arr1[0, 0] = 2;
+//            //arr1[0, 1] = 3;
+//            //arr1[1, 0] = 7;
+//            //arr1[1, 1] = 6;
+//            //for (i = 0; i < 2; i++)
+//            //{
+//            //    for (j = 0; j < 2; j++)
+//            //    {
+//            //        Console.Write(arr1[i, j] + "  ");
+//            //    }
+//            //    Console.WriteLine();
+//            //}
+//            //int a = 0;
 
-//                    }
-//                }
-//            Console.WriteLine("Addition of all elements in the matrix is: " + b);
+//            //for (i = 0; i <= 1; i++)
+//            //{
+
+//            //    a = arr1[i, i] + a;
+
+//            //}
+//            //Console.WriteLine("Addition of principal diagonal elements is: " + a);
+//            //int b = 0;
+//            //for (i = 0; i <= 1; i++)
+//            //{
+//            //    for (j = 0; j <= 1; j++)
+//            //    {
+//            //        b = arr1[i, j] + b;
+
+//            //    }
+//            //}
+//            //Console.WriteLine("Addition of all elements in the matrix is: " + b);
 
 //            Console.ReadLine();
-          
+
 //        }
-        
+
 //    }
-    
+
 //}
