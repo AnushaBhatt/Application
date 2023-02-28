a937a5180a8fff382914f86665b9caa1a5e9547f
Anusha <Anusha.Bhattacharya@gds.ey.com>
2023-02-28 08:08:07 +0000

    Added once more

--- EYPrograms/EYPrograms/Class11.cs
+++ EYPrograms/EYPrograms/Class11.cs
@@ -1,123 +1,201 @@
-﻿////using System;
-////using System.Collections.Generic;
-////using System.Linq;
-////using System.Text;
-////using System.Threading.Tasks;
+﻿////////using System;
+////////using System.Collections.Generic;
+////////using System.Linq;
+////////using System.Text;
+////////using System.Threading.Tasks;
 
-////namespace EYPrograms
+////////namespace EYPrograms
+////////{
+////////    class india
+////////    {
+////////        static int custid = 100;
+////////        static string custname;
+
+////////        public void display()//by default all member functions are private
+////////        {
+////////            Console.WriteLine("Customer id is: " + custid);
+////////        }
+////////    }//if we keep void display it is not able to access elements 
+////////    class UK
+////////    {
+////////        protected int empid;
+////////        protected string empname;
+////////    }
+////////    class US
+////////    {
+////////        public int studid=10;
+////////        public string studname;
+////////    }
+////////    class Class11
+////////    {
+////////        static void Main(string[] args)
+////////        {
+////////           india obj1 = new india();
+////////          obj1.display();
+
+////////           // UK obj2 = new UK();
+////////           // obj2.//cannot access
+
+////////                US obj3 = new US();
+////////            obj3.studid = 7 ;
+////////            Console.ReadLine();
+////////        }
+////////    }
+////////}
+////////single level inheritance
+//////using System;
+
+//////class vehicle // parent class 
+//////{
+//////    public void vehiclecolor()
+//////    {
+//////        Console.WriteLine("car colors available are : green, blue, red");
+
+//////    }
+//////}
+//////class car : vehicle  //hierarchial inheritance
+//////{
+//////    public void musicsystem()
+//////    {
+//////        Console.WriteLine("Adding music system inside jeep");
+//////    }
+//////}
+//////class scooter : car //scooter is child class and its multilevel inheritence
+//////{
+//////    public void wheel()
+//////    {
+//////        Console.WriteLine("car has two wheels");
+//////    }
+//////}
+//////class jeep : car  //car is child class
+//////{
+//////    public void carcolor()
+//////    {
+//////        Console.WriteLine("car colors available are : blue, red");
+//////    }
+//////}
+//////class myclass
+//////{
+//////    static void Main(string[] args)
+//////    {
+//////        jeep obj = new jeep();
+//////        obj.carcolor();
+//////        obj.vehiclecolor();
+//////        obj.musicsystem();
+//////        Console.ReadLine();
+//////    }
+//////}
+
+//////access specifiers with inheritence
+//////using System;
+//////class india
+//////{
+//////    protected indiaid = 34;
+//////        public void function1()
+//////    {
+//////        Console.WriteLine("india class--function1()-- indiaid is " + indiaid);
+
+//////    }
+
+//////}
+//////class bangalore : india
+//////{
+//////    int bangid = 10;
+//////    public void function2()
+//////    {
+//////        Console.WriteLine("bangalore class--function2()-- bangid is " + bangid);
+//////    }
+//////}
+//////class tumkur : bangalore
+//////{
+//////    int tumkurid = 10;
+//////    public void function3()
+//////    {
+//////        Console.WriteLine("tumkur class--function3()-- tumkurid is " + tumkurid);
+//////    }
+//////}
+//////class mysore : india
+//////{
+//////    static void Main(string[] args)
+//////    {
+
+//////    }
+//////}
+//////function overloading
+//////using System;
+//////class calc
+//////{
+//////    public int add(int a, int b)
+//////    {
+//////       // Console.WriteLine("Addition result is: "+(a+b);
+//////       return a + b;
+//////    }
+//////    public int add(int a, int b, int c)
+//////    {
+//////        //  Console.WriteLine("Addition of 3 numbers is: " +(a+b+c));
+//////        return a + b + c;
+//////    }
+//////    public int add(int a)
+//////    {
+//////        //Console.WriteLine("Addition of numbers is: " + (a + a));
+//////        return a + a;
+//////    }
+//////}
+
+//////class myclass
+//////{
+//////    static void Main(string[] args)
+//////    {
+//////        calc obj = new calc();
+//////        Console.WriteLine(obj.add(2, 4));
+//////        Console.WriteLine(obj.add(10));
+//////        Console.WriteLine(obj.add(4, 4, 2));
+//////        Console.ReadLine();
+//////    }
+//////}
+////class complex
 ////{
-////    class india
-////    {
-////        static int custid = 100;
-////        static string custname;
+////    private int x;
+////    private int y;
 
-////        public void display()//by default all member functions are private
+////    public Complex() //default
+////    { }
+////    public Complex(int i, int  j)
+////        public void 
+////        class MyClient
+////    {
+////        public static void Main()
 ////        {
-////            Console.WriteLine("Customer id is: " + custid);
-////        }
-////    }//if we keep void display it is not able to access elements 
-////    class UK
-////    {
-////        protected int empid;
-////        protected string empname;
-////    }
-////    class US
-////    {
-////        public int studid=10;
-////        public string studname;
-////    }
-////    class Class11
-////    {
-////        static void Main(string[] args)
-////        {
-////           india obj1 = new india();
-////          obj1.display();
+////            Complex c1 = new Complex(10, 20);
+////            c1.ShowXY();//displays 10 & 20
+////            Complex c2 = new Complex();
+////            c2.ShowXY();//displays 0 & 0
 
-////           // UK obj2 = new UK();
-////           // obj2.//cannot access
-
-////                US obj3 = new US();
-////            obj3.studid = 7 ;
+////            c2 = -c1;
+////            c2.ShowXY(); //displays -10 &-20
 ////            Console.ReadLine();
+
+
 ////        }
 ////    }
+
+
+
 ////}
-////single level inheritance
+////function overriding
 //using System;
-
-//class vehicle // parent class 
-//{
-//    public void vehiclecolor()
-//    {
-//        Console.WriteLine("car colors available are : green, blue, red");
-
-//    }
-//}
-//class car : vehicle  //hierarchial inheritance
-//{
-//    public void musicsystem()
-//    {
-//        Console.WriteLine("Adding music system inside jeep");
-//    }
-//}
-//class scooter : car //scooter is child class and its multilevel inheritence
-//{
-//    public void wheel()
-//    {
-//        Console.WriteLine("car has two wheels");
-//    }
-//}
-//class jeep : car  //car is child class
-//{
-//    public void carcolor()
-//    {
-//        Console.WriteLine("car colors available are : blue, red");
-//    }
-//}
 //class myclass
 //{
-//    static void Main(string[] args)
+//    public virtual void function1()
 //    {
-//        jeep obj = new jeep();
-//        obj.carcolor();
-//        obj.vehiclecolor();
-//        obj.musicsystem();
-//        Console.ReadLine();
+//        Console.WriteLine("myclass----function1()");
 //    }
 //}
-
-//access specifiers with inheritence
-using System;
-class india
-{
-    protected indiaid = 34;
-        public void function1()
-    {
-        Console.WriteLine("india class--function1()-- indiaid is " + indiaid);
-
-    }
-
-}
-class bangalore : india
-{
-    int bangid = 10;
-    public void function2()
-    {
-        Console.WriteLine("bangalore class--function2()-- bangid is " + bangid);
-    }
-}
-class tumkur : bangalore
-{
-    int tumkurid = 10;
-    public void function3()
-    {
-       Console.WriteLine("tumkur class--function3()-- tumkurid is " + tumkurid);
-    }
-}
-class mysore: india
-{
-    static void Main(string[] args)
-    {
-
-    }
-}
+//class india: myclass
+//{
+//    public override void function1()
+//    {
+//        Console.WriteLine("india----function1() ");
+//        //base.function1();
+//    }
+//}
---
+++ EYPrograms/EYPrograms/Class12.cs
@@ -0,0 +1,274 @@
+﻿////////////////using System;
+////////////////using System.Collections.Generic;
+////////////////using System.Linq;
+////////////////using System.Text;
+////////////////using System.Threading.Tasks;
+//////////////////implicit interface implementation
+////////////////namespace EYPrograms
+////////////////{
+
+////////////////    public void remove()
+////////////////    {
+////////////////        Console.WriteLine("")
+////////////////    }
+////////////////    class Class12
+////////////////    {
+////////////////        static void Main(string[] args)
+////////////////        {
+////////////////            myclass m1 = new myclass();
+////////////////            m1.add();
+////////////////            m1.remove();
+////////////////            m1.clear();
+////////////////        }
+////////////////    }
+////////////////}
+////////////////explicit implementation
+//////////////using System;
+
+//////////////class myclass : 
+//////////////    class Day11
+//////////////{
+//////////////    static void Main(string[] args)
+//////////////    {
+//////////////        //myclass m1= new myclass();
+//////////////        Imyinterface m2 = new myclass();
+//////////////        m2.add();
+//////////////        m2.remove();
+//////////////        Inewinterface m3 = new myclass();
+//////////////        m3.clear();
+
+//////////////        //Imyinterface
+//////////////    }
+//////////////}
+//////////////sealed keyword which helps to stop the inheritance
+////////////using System;
+////////////sealed class A
+////////////{
+////////////    public void function1()
+////////////    {
+////////////        Console.WriteLine("A -- function1()");
+
+////////////    }
+////////////}
+////////////class myclass
+////////////{
+////////////    static void Main(string[] args)
+////////////    {
+////////////        A obj = new A();
+////////////        obj.function1();
+////////////        Console.ReadLine();
+////////////    }
+////////////}
+////////////constructor
+//////////using System;
+//////////class myclass
+//////////{
+//////////    int k = 10;
+//////////    int j = 20;
+//////////    public myclass()
+//////////    {
+//////////        Console.WriteLine("Default constructor is invoked");
+//////////    }
+//////////    public myclass(int a, int b)
+//////////    {
+//////////        k = a;
+//////////        j = b;
+
+//////////    }
+//////////    public void display()
+//////////    {
+//////////        Console.WriteLine("k value is: " + k);
+//////////        Console.WriteLine("j value is: " + j);
+//////////    }
+//////////}
+//////////class newclass
+//////////{
+//////////    static void Main(string[] args)
+//////////    {
+
+//////////    }
+//////////}
+//////////copy constructor
+////////using System;
+////////class employee
+////////{
+////////    private string name;
+////////    private int age;
+
+////////    public employee(employee emp)
+////////    {
+////////        name = emp.name;
+////////        age = emp.age;
+
+////////    }
+////////    public employee(string name, int age)
+////////    {
+////////        this.name = name;
+////////        this.age = age;
+////////    }
+////////    public void display()
+////////    {
+////////        Console.WriteLine("Name is: " + name);
+////////        Console.WriteLine("Age is: " + age);
+////////    }
+////////}
+////////class newE
+////////{
+////////    static void Main(string[] args)
+////////    {
+
+////////    }
+////////}
+//////using System;
+//////class Geeks
+//////{
+//////    //variables
+//////    public static string name;
+//////    public static int num;
+//////    //creating private constructor using private keyword
+//////    private Geeks()//not accesible
+//////    {
+//////        Console.WriteLine(" Default constructor is invoked");
+//////    }
+//////    //default constructor with parameters
+//////    public Geeks(string a, int b)
+//////    {
+//////        name = a;
+//////        num = b;
+//////    }
+
+//////}
+////////driver class
+//////class GFG
+//////{
+//////    static void Main(string[] args)
+//////    {
+//////        //this line raises error because the constructor is inaccesible
+//////        Geeks obj2 = new Geeks("Anusha", 23);
+//////        Console.WriteLine(Geeks.name + " , " + Geeks.num);
+//////        Console.ReadLine();
+//////    }
+//////}
+//////static constructor
+////using System;
+////class Account
+////{
+////    public int id;
+////    public string name;
+////    public static float rateOfInterest;
+////    public Account(int id, String name)
+////    {
+////        this.id = id;
+////        this.name = name;
+////        //this.rate
+////    }
+////    static Account()
+////    {
+////        rateOfInterest = 9.5f;
+
+////    }
+////    public void display()
+////    {
+////        Console.WriteLine(id + " " + name + " " + rateOfInterest);
+////    }
+////}
+
+////class TestEmployee
+////{
+////    public static void Main(string[] args)
+////    {
+////        Account a1 = new Account(101, "Harshit");
+////        Account a2 = new Account(102, "Anusha");
+
+////        a1.display();
+////        a2.display();
+////        Console.ReadLine();
+////    }
+////}
+
+////abstract class
+//using System;
+//abstract class myclass1
+//{
+//    public abstract void function1();
+//    public abstract void function2();
+//    public void function3()
+//    {
+//        Console.WriteLine("Welcome to abstract classes");
+//    }
+//}
+
+//class  US : myclass1
+//{
+//    public override void function1()
+//    {
+//        Console.WriteLine("Us class--- function1()");
+//    }
+//    public override void function2()
+//    {
+//        Console.WriteLine("US class---function2()");
+//    }
+//}
+
+//class india
+//{
+//    static void Main(string[] args)
+//    {
+//        US obj = new US();
+//        obj.function1();
+//        obj.function2();
+//        obj.function3();
+//        Console.ReadLine();
+//    }
+//}
+//upcasting and downcasting
+using System;
+public class shape
+{
+    protected int m_xpos;
+    protected int m_ypos;
+}
+
+public class square: shape
+{
+    public square()
+    {
+    }
+    public square(int x, int y): base(x,y)
+    { }
+    public new void draw()
+    {
+        Console.WriteLine("drawing a square at {0},{1}", m_xpos, m_ypos);
+    }
+}
+
+public class circle : shape
+{
+    public circle()
+    { }
+    public circle(int x, int y): base(x,y)
+    { }
+    public new void draw()
+    {
+        Console.WriteLine("drawing a circle at {0},{1}", m_xpos, m_ypos);
+    }
+}
+public void draw
+{
+    Console.WriteLine("Drawing a shape at {0},{1}" );
+        static void Main(string[] args)
+{
+    shape sh = new shape(100, 100);
+    sh.draw();
+    square sq = new square(200, 200);
+    sq.draw();
+    circle ci = new circle(300, 300);
+    ci.draw();
+
+    shape[] shapes = new shape[3];
+    shapes[0] = new shape(100, 100);
+    shapes[1] = new square(200, 200);
+    shapes[2]= new circle(300, 300);
+
+    foreach()
+}
