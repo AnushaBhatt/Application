1879464d31670aae6bb74604e1b51abbb1c724f7
Anusha <Anusha.Bhattacharya@gds.ey.com>
2023-03-17 06:57:51 +0000

    next program added

---
+++ ConsoleApp1/ConsoleApp1/Emp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+
+
+        public int empid { get; set; }
+        public string empname { get; set; }
+        public char empgender { get; set; }
+        public int empsalary { get; set; }
+
+
+    }
+    class CFG
+    {
+        static public void Main()
+        {
+            List<Employee> emp = new List<Employee>()
+            {
+             new Employee() { empid= 1, empname= "anusha", empgender= 'f', empsalary=3400 },
+             new Employee() { empid= 1, empname= "harsh", empgender= 'm', empsalary=5000 },
+            new Employee() { empid = 1, empname = "anvita", empgender = 'f', empsalary = 7400 },
+            new Employee() { empid = 1, empname = "bhoomika", empgender = 'f', empsalary = 3000 }
+
+            };
+
+            //count the total number of employees
+            var res = (from i in emp
+                       select i.empid).Count();
+            Console.WriteLine("Empids are: " + res);
+            var res1 = (from i in emp
+                       select i.empsalary).Max();
+            Console.WriteLine("Maximum salary: " + res1 );
+            var res2 = (from i in emp
+                        select i.empgender).Count();
+            Console.WriteLine("Number of female: " + res2);
+
+
+            Console.ReadLine();
+        }
+       
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,79 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace ConsoleApp1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            //string[] arr = { "anusha", "harshit", "bhoomika", "anvita" };
-            //var query1 = from i in arr
-            //             select i;
-            //Console.WriteLine("Enter names of students");
-            //foreach(var item in query1)
-            //{
-            //    Console.WriteLine(item);
-            //}
+//            //string[] arr = { "anusha", "harshit", "bhoomika", "anvita" };
+//            //var query1 = from i in arr
+//            //             select i;
+//            //Console.WriteLine("Enter names of students");
+//            //foreach(var item in query1)
+//            //{
+//            //    Console.WriteLine(item);
+//            //}
 
-            //var query2 = from i in arr
-            //             where i.Contains('a')
-            //             select i;
+//            //var query2 = from i in arr
+//            //             where i.Contains('a')
+//            //             select i;
 
-            //Console.WriteLine("Enter names of students contains 'a' ");
-            //foreach (var item in query2)
+//            //Console.WriteLine("Enter names of students contains 'a' ");
+//            //foreach (var item in query2)
 
-            //{
-            //    Console.WriteLine(item);
-            //}
+//            //{
+//            //    Console.WriteLine(item);
+//            //}
 
-            //var query3 = from i in arr
-            //             where i.StartsWith("a")
-            //             select i;
-            //Console.WriteLine("Enter names of students starts with 'a' ");
-            //foreach (var item in query3)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //var query5 = from i in arr
-            //             where i.Length==4
-            //             select i;
-            //Console.WriteLine("Enter names of students" );
-            //foreach (var item in query5)
-            //{
-            //    Console.WriteLine(item);
-            //}
+//            //var query3 = from i in arr
+//            //             where i.StartsWith("a")
+//            //             select i;
+//            //Console.WriteLine("Enter names of students starts with 'a' ");
+//            //foreach (var item in query3)
+//            //{
+//            //    Console.WriteLine(item);
+//            //}
+//            //var query5 = from i in arr
+//            //             where i.Length==4
+//            //             select i;
+//            //Console.WriteLine("Enter names of students" );
+//            //foreach (var item in query5)
+//            //{
+//            //    Console.WriteLine(item);
+//            //}
 
 
 
-            //var query4 = from i in arr
-            //             where i.Distinct<>
+//            //var query4 = from i in arr
+//            //             where i.Distinct<>
 
-            //             select i;
-            //Console.WriteLine("Enter names of students");
-            //foreach (var item in query1)
-            //{
-            //    Console.WriteLine(item);
-            //}
+//            //             select i;
+//            //Console.WriteLine("Enter names of students");
+//            //foreach (var item in query1)
+//            //{
+//            //    Console.WriteLine(item);
+//            //}
             
-            int[] arr = { 1, 5, 7, 23, 76, 98, 90, 35, 28, 67 };
-            var query1 = from i in arr
-                             // where i >= 40
-                        where i >7 && i<=100 
-                        
-                         select i;
-            foreach(var item in query1)
-            {
-                Console.WriteLine(item);
-            }
+//            double[] arr = { 1, 5, 7, 23, 76, 98, 90, 35, 28, 67 };
+//            //var query1 = from i in arr
+//            //                 // where i >= 40
+//            //            where i >7 && i<=100 
 
-            Console.ReadLine();
-        }
+//            //             select i;
+//            //foreach(var item in query1)
+//            //{
+//            //    Console.WriteLine(item);
+//            //}
+//            int result1 = arr.Count();
+//            Console.WriteLine("Count is: " + result1);
+
+//            double result2 = arr.Max();
+//            Console.WriteLine("Max is: " + result2);
+
+//            int result3 = arr.Min();
+//            Console.WriteLine("Min is: " + result3);
+
+//            int result4 = arr.Average();
+//            Console.WriteLine("Count is: " + result4);
+
+//            Console.ReadLine();
+//        }
        
-    }
+//    }
     
-}
+//}
