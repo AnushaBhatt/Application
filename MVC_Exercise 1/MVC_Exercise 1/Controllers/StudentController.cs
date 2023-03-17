449d33d002cbab920b0387c34ddc0ee3395cf60c
Anusha <Anusha.Bhattacharya@gds.ey.com>
2023-03-17 10:14:26 +0000

    new program mvc

---
+++ MVC_Exercise 1/MVC_Exercise 1/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Exercise_1.Models;
+namespace MVC_Exercise_1.Controllers
+{
+    public class StudentController : Controller
+    {
+        IList<student> studdetails = new List<student>()
+            {
+             new student() { studid= 1, studname= "anusha", studmarks=400 },
+             new student() { studid= 1, studname= "harsh", studmarks=200 },
+            new student() { studid = 1, studname = "anvita", studmarks=400 }
+
+
+            };
+        public ActionResult StudentList ()
+
+        {
+
+
+            //ViewBag.details = studdetails;
+            //return View();
+            ViewData["details"] = studdetails;
+            return View();
+        }
+    }
+}
---
+++ MVC_Exercise 1/MVC_Exercise 1/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Exercise_1
+{
+    public class Student
+    {
+        public int studid { get; set; }
+
+        public string studname { get; set; }
+
+        public int studmarks { get; set; }
+    }
+
+    
+}
